7f0315da518b7d7c1d299aff9b397d73dd2689c4
Heni <i@pingas.org>
2025-06-24 22:27:58 -0500

    feat: allow admin override users to trigger any interaction

--- src/services/AccessControlService.ts
+++ src/services/AccessControlService.ts
@@ -3,14 +3,20 @@ import { configService } from "./ConfigService.ts";
 class AccessControlService {
     private readonly allowedUsers: Set<string>;
     private readonly isWhitelist: boolean;
+    private readonly adminOverrideUsers: Set<string>;
 
     constructor() {
         const userIdList = configService.getUserIdList();
         this.allowedUsers = new Set(userIdList.split(";").filter((id: string) => id.trim() !== ""));
         this.isWhitelist = configService.isWhitelistEnabled();
+        this.adminOverrideUsers = new Set(configService.getAdminOverrideList());
     }
 
     public isUserAllowed(userId: string): boolean {
+        if (this.adminOverrideUsers.has(userId)) {
+            return true;
+        }
+
         if (this.allowedUsers.size === 0) {
             return true;
         }
