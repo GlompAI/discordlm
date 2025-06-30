74fda62f352182505dd524c866edaa2481dd9b6e
Heni <i@pingas.org>
2025-06-30 05:31:47 -0500

    fix: use node import for https

--- src/tools.ts
+++ src/tools.ts
@@ -1,4 +1,4 @@
-import * as https from "https";
+import * as https from "node:https";
 import { FunctionDeclaration, SchemaType } from "@google/generative-ai";
 import { getJinaApiKey } from "./env.ts";
 
